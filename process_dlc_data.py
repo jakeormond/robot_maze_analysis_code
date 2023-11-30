a519a5ddb9a6924c8eee51fef40ed9ea29fab6d1
Jake Ormond <j.ormond@ucl.ac.uk>
2023-11-30 21:49:37 +0000

    minor change

--- process_dlc_data.py
+++ process_dlc_data.py
@@ -6,3 +6,14 @@ from numpy import matlib as mb
 import re
 import pickle
 import pycircstat
+
+# set working directory
+ome_dir = 'D:/analysis' # WINDOWS
+# home_dir = "/media/jake/LaCie/data_analysis" # Linux/Ubuntu
+os.chdir(home_dir)
+
+animal = 'Rat64'
+session = '08-11-2023'
+data_dir = os.path.join(home_dir, animal, session)
+
+dlc_dir = os.path.join(data_dir, 'deeplabcut')
